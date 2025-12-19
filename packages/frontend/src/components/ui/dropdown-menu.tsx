0b3aaad39745bd6482ce90ea25a3624d1bf2a638
Ihor Bydzinskyi <igor.budzin95@gmail.com>
2025-12-19 14:34:13 +0200

    Remove @radix-ui packages

--- packages/frontend/src/components/ui/dropdown-menu.tsx
+++ packages/frontend/src/components/ui/dropdown-menu.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
+import { DropdownMenu as DropdownMenuPrimitive } from "radix-ui"
 import { Check } from "../icons/Check"
 import { ChevronRight } from "../icons/ChevronRight"
 import { Circle } from "../icons/Circle"
